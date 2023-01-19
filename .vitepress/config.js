26cc28f1e6882836583a62d04777b3a9528ce8fa
a-wing <1@233.email>
2023-01-19 23:31:32 +0800

    refactor add use vitepress

---
+++ .vitepress/config.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'OutLife',
+  description: 'A Minecraft Server',
+  themeConfig: {
+    logo: '/img/outlife.png',
+    nav: [
+      { text: 'Map', link: 'https://map.outlife.world' },
+      { text: 'Telegram', link: 'https://t.me/outlifemc' },
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/outlife' },
+    ],
+  }
+}
